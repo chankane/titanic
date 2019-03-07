bbed775f308771088d049ec5a53d28f4560559b3
fj181435 <kanegawa.masaya@jp.fujitsu.com>
2019-03-07 17:35:30 +0900

    20190307

--- filling/fare.R
+++ filling/fare.R
@@ -1,18 +1,28 @@
 # 線形回帰分析で補間
+
 source("util/util.R")
+
+
+# 行数優先
+ManyRow <- function(df) {
+  df <- df[c("Fare", "Pclass", "SibSp", "Parch", "Female", "C")]
+  df[is.na(df ]
+}
 
 
 df <- ReadData()
 
-Fare <- df$Fare  # df$Fare を直接下に代入すると、df$Fare という名前の列ができる
-df.x = cbind(DropColumnsOfNa(df), Fare)
+head(ManyRow(df))
 
-model <- lm(df$Fare ~ ., data=df.x)
+#Fare <- df$Fare  # df$Fare を直接下に代入すると、df$Fare という名前の列ができる
+#df.x = cbind(DropColumnsOfNa(df), Fare)
+
+#model <- lm(df$Fare ~ ., data=ManyRow(df))
 
 na.idx <- is.na(df$Fare)
 
-pred = predict(model, df[na.idx, ])
+#pred = predict(model, df[na.idx, ])
 
-df[na.idx, ] = pred
+#df[na.idx, ] = pred
 
-WriteData(df)
+#WriteData(df)
